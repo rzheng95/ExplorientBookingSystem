46eedc7a0bd3704d7579e763b06796da2c8d1c2c
rzheng95 <ruixiangzheng95@gmail.com>
2020-02-09 20:06:13 -0500

    Added docx.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { User } from '../../models/user.model';
 
+import { Packer } from 'docx';
+// import { saveAs } from 'file-saver';
+
+import saveAs from 'file-saver';
+
+
+import { experiences, education, skills, achievements } from './cv-data';
+import { DocumentCreator } from './cv-generator';
+
+
 
 @Component({
   selector: 'app-dashboard',
@@ -16,13 +26,22 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.user = this.authService.getUserData();
-
-
-
   }
 
   onDownload() {
+    const documentCreator = new DocumentCreator();
+    const doc = documentCreator.create([
+      experiences,
+      education,
+      skills,
+      achievements
+    ]);
 
+    Packer.toBlob(doc).then(blob => {
+      console.log(blob);
+      saveAs(blob, 'example.docx');
+      console.log('Document created successfully');
+    });
   }
 
   onLogout() {
